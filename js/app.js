d3e45e85994e7ea2c49c7fd1ca36f10d00fe8157
Yiou Chen <chen.yiou1992@gmail.com>
2015-05-17 02:52:57 -0400

    move main page layout to custom element

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 var app;
 (function (document) {
     'use strict';
-    app=document.querySelector('#app');
-    app.toggle1=function(e){
-        app.col1=!app.col1;
-        app.col2=false;
-    }
-    app.toggle2=function(e){
-        app.col1=false;
-        app.col2=!app.col2;
-    }
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+    app = document.querySelector('#app');
+    
+    document.addEventListener('next_page', function (e) {
+            var pages = document.querySelector('core-pages');
+            pages.selected = (pages.selected + 1) % pages.children.length;
+        });
+        // wrap document so it plays nice with other libraries
+        // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
