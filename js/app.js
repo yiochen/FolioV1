498d86ed3150de640cfcf39be87799d4c7eda12c
Yiou Chen <chen.yiou1992@gmail.com>
2015-05-22 01:51:09 -0400

    use template on projects

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
 var app;
+var data;
 (function (document) {
     'use strict';
     app = document.querySelector('#app');
-    
+    app.project=document.querySelector('#project');
     document.addEventListener('next_page', function (e) {
-            var pages = document.querySelector('core-pages');
-            pages.selected = (pages.selected + 1) % pages.children.length;
-        });
+        var pages = document.querySelector('core-pages');
+        pages.selected = (pages.selected + 1) % pages.children.length;
+    });
+    //    var request = new XMLHttpRequest();
+    //    request.open('GET', '/data/projects.json', true);
+    //    request.onload = function () {
+    //        alert("haha");
+    //        if (request.status >= 200 && request.status < 400) {
+    //            // Success!
+    //            data = JSON.parse(request.responseText);
+    //            
+    //        } else {
+    //            // We reached our target server, but it returned an error
+    //
+    //        }
+    //
+    //    };
+    //    request.send();
+    app.handleProjectData = function (eve) {
+
+            app.project.response = eve.detail.response;
+            console.log(app.project.response);
+            
+        }
         // wrap document so it plays nice with other libraries
         // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
