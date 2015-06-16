1310005e1e114274e7b0ee004a230b56b2b2f96a
Yiou Chen <chen.yiou1992@gmail.com>
2015-06-15 23:24:48 -0400

    gulp autoprefix css and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 var gulp=require('gulp');
 var vulcanize=require('gulp-vulcanize');
 var connect=require('gulp-connect');
+var minifyCss=require('gulp-minify-css');
+var autoprefixer=require('gulp-autoprefixer');
 
 gulp.task('vul',function(){
   return gulp.src('elements.html')
   .pipe(vulcanize({
     abspath:'',
-    excludes:[],
-    stripExcludes:false
+    stripComments:true
   }))
   .pipe(gulp.dest('build'));
+});
+gulp.task('image',function(){
+  return gulp.src('images/**/*')
+  .pipe(gulp.dest('build/images'));
+})
+gulp.task('css',function(){
+  return gulp.src('{css,elements}/*.css',{base:'{css,elements}'})
+  .pipe(autoprefixer({
+    browsers:['last 2 versions'],
+    cascade:false,
+  }))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('serve',function(){
